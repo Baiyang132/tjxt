db5808a321ab9ebf4d8b01031e833c1f935f4957
Bya <1326393476@qq.com>
2026-09-13 21:02:30 +0800

    优惠卷02

--- tj-promotion/src/main/java/com/tianji/promotion/service/impl/CouponServiceImpl.java
+++ tj-promotion/src/main/java/com/tianji/promotion/service/impl/CouponServiceImpl.java
@@ -1,17 +1,21 @@
 package com.tianji.promotion.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tianji.common.domain.dto.PageDTO;
 import com.tianji.common.exceptions.BadRequestException;
 import com.tianji.common.utils.BeanUtils;
 import com.tianji.common.utils.CollUtils;
 import com.tianji.promotion.domain.dto.CouponFormDTO;
 import com.tianji.promotion.domain.po.Coupon;
-import com.tianji.promotion.domain.po.CouponScope;
+import com.tianji.promotion.domain.query.CouponQuery;
+import com.tianji.promotion.domain.vo.CouponPageVO;
 import com.tianji.promotion.service.ICouponScopeService;
 import com.tianji.promotion.service.ICouponService;
 import com.tianji.promotion.mapper.CouponMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.tianji.common.utils.StringUtils;
 
 import java.util.List;
 
@@ -55,6 +59,30 @@ public class CouponServiceImpl extends ServiceImpl<CouponMapper, Coupon>
 
 
 
+    }
+
+    @Override
+    public PageDTO<CouponPageVO> queryCouponByPage(CouponQuery query) {
+        //1.获取优惠券的状态
+        Integer status = query.getStatus();
+        //获取优惠券的名称
+        String name = query.getName();
+        //获取优惠券的类型
+        Integer type = query.getType();
+        //2.分页查询
+        Page<Coupon> page = lambdaQuery()
+                .eq(type != null, Coupon::getType, type)
+                .eq(status != null, Coupon::getStatus, status)
+                .like(StringUtils.isNotBlank(name), Coupon::getName, name)
+                .page(query.toMpPageDefaultSortByCreateTimeDesc());
+        //3.处理VO
+        List<Coupon> records = page.getRecords();
+        if (CollUtils.isEmpty(records))
+            return PageDTO.empty(page);
+
+        List<CouponPageVO> list = BeanUtils.copyList(records, CouponPageVO.class);
+
+        return PageDTO.of(page, list);
     }
 }
 
